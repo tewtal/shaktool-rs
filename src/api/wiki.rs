62275fa94100fb931d3e3f4f86c9a354322f798c
tewtal <total@viskos.org>
2021-10-27 17:42:04 +0200

    Leaderboard things

--- src/api/wiki.rs
+++ src/api/wiki.rs
@@ -1,25 +1,30 @@
 use mediawiki::api::Api;
 use mediawiki::title::Title;
 use std::error::Error;
+use std::collections::VecDeque;
 use kuchiki::traits::*;
+use cached::proc_macro::cached;
 
 const URL: &str = "https://wiki.supermetroid.run/api.php";
 
 
 pub type WikiResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
 
+#[derive(Debug, PartialEq)]
 pub struct WikiRecord {
-    place: i32,
-    runner: String,
-    real_time: String,
-    date: String,
-    link: String,
-    source: String,
-    comment: String,
-    category: String
+    pub place: i32,
+    pub runner: String,
+    pub real_time: String,
+    pub date: String,
+    pub link: String,
+    pub source: String,
+    pub comment: String,
+    pub category: String
 }
 
-pub async fn get_wiki_leaderboard() -> WikiResult<Vec<WikiRecord>> {
+/* Use the cached crate here to cache this for 300 seconds (5 minutes) to prevent hammering of the Wiki */
+#[cached(size=1, time=300, result=true)]
+pub async fn get_leaderboard_result() -> WikiResult<serde_json::Value> {
     let api = Api::new(URL).await?;
     let params = api.params_into(&[
         ("action", "parse"),
@@ -27,32 +32,71 @@ pub async fn get_wiki_leaderboard() -> WikiResult<Vec<WikiRecord>> {
         ("prop", "text")
     ]);
 
-    println!("Starting query...");
     let result = api.get_query_api_json(&params).await?;
-    let text = result.get("parse").unwrap().get("text").unwrap().get("*").unwrap().as_str().unwrap();
-    let document = kuchiki::parse_html().one(text);
-    println!("Got response");
+    Ok(result)
+}
 
-    for row in document.select("tr").unwrap() {
-        let node = row.as_node();
-        
+pub async fn get_wiki_leaderboard() -> WikiResult<Vec<WikiRecord>> {
+
+    let result = get_leaderboard_result().await?;
+    let text = result.get("parse").ok_or("Could not parse wiki result")?
+        .get("text").ok_or("Could not parse wiki result")?
+        .get("*").ok_or("Could not parse wiki result")?
+        .as_str().ok_or("Could not parse wiki result")?;
+
+    let document = kuchiki::parse_html().one(text);
+
+    let mut categories = VecDeque::new();
+    let mut records = Vec::new();
+
+    /* Get all categories */
+    for category in document.select("h1 > span.mw-headline > a").map_err(|_| "Could not parse categories")? {
+        if let Some(element) = category.as_node().as_element() {
+            let attributes = element.attributes.borrow();
+            if let Some(category_name) = attributes.get("title") {
+                categories.push_back(category_name.to_string());
+            }
+        }
     }
 
-    // for row in html.select(&row_selector) {
-    //     //let mut cols = row.select(&col_selector);
-    //     println!("{:?}", cols);
-    //     break;
-    // }
+    /* Parse the tables */
+    for table in document.select("table").map_err(|_| "Could not parse tables")? {
+        let category = categories.pop_front().unwrap_or("Unknown".to_string());
+        let table_node = table.as_node();
+        for tr in table_node.select("tr").map_err(|_| "Could not parse rows")? {
+            let tr_node = tr.as_node();
+            let cols = tr_node.select("td").map_err(|_| "Could not parse columns")?.map(|e| e.text_contents().trim().to_string()).collect::<Vec<_>>();
+            if cols.len() > 0 {
+                let place = i32::from_str_radix(&cols[0], 10).unwrap_or(999);
+                
+                let link = match tr_node.select("a") {
+                    Ok(mut l) => {
+                        if let Some(node) = l.next() {
+                            let attributes = node.attributes.borrow();
+                            let link_href = attributes.get("href").unwrap_or("");
+                            link_href.to_string()
+                        } else {
+                            String::default()
+                        }
+                    }
+                    _ => String::default()
+                };
 
-    //let mut rows = html.select(&row_selector);
-    // rows.next();
-    // rows.next();
+                records.push(WikiRecord {
+                    place,
+                    runner: cols[1].clone(),
+                    real_time: cols[2].clone(),
+                    date: cols[3].clone(),
+                    link,
+                    source: cols[5].clone(),
+                    comment: cols[6].clone(),
+                    category: category.clone()
+                });
+            }
+        }
+    }
 
-    //let row = rows.next().unwrap();
-    //let cols = row.select(&col_selector).next().unwrap();
-
-
-    Ok(Vec::new())
+    Ok(records)
 }
 
 pub async fn search_wiki_titles(title: &str) -> WikiResult<Vec<Title>>  {
--- src/commands/leaderboard.rs
+++ src/commands/leaderboard.rs
@@ -5,7 +5,7 @@ use crate::api::wiki;
 
 
 #[group]
-#[commands(top)]
+#[commands(top, records)]
 struct Leaderboard;
 
 #[command]
@@ -14,6 +14,54 @@ struct Leaderboard;
 #[usage = "<category>"]
 #[example = "any%"]
 pub async fn top(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    let records = wiki::get_wiki_leaderboard().await;
+    let records = wiki::get_wiki_leaderboard().await?;
+    let category = args.message().to_lowercase();    
+    
+    if !records.is_empty() {
+        let mut display_category = String::new();
+        let mut output = String::new();
+
+        for record in records.iter().filter(|r| r.category.to_lowercase().contains(&category)).take(10) {
+            output.push_str(&format!("({}) **{}** by **{}** :: <{}>\n", record.place, record.real_time, record.runner, record.link));
+            
+            if display_category.is_empty() {
+                display_category = record.category.to_string();
+            }
+        }
+
+        msg.channel_id.say(&ctx, &format!("Top records for: **{}**\n{}", display_category, output)).await?;
+    } else {
+        msg.channel_id.say(&ctx, &format!("No records found for category: **{}**", category)).await?;
+    }
+
+    Ok(())
+}
+
+#[command]
+#[description = "Lists all records for a given player"]                
+#[min_args(1)]
+#[usage = "<player>"]
+#[example = "total"]
+pub async fn records(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let records = wiki::get_wiki_leaderboard().await?;
+    let runner = args.message().to_lowercase();    
+    
+    if !records.is_empty() {
+        let mut display_name = String::new();
+        let mut output = String::new();
+
+        for record in records.iter().filter(|r| r.runner.to_lowercase().contains(&runner)) {
+            output.push_str(&format!("**{}** ({}) **{}** :: <{}>\n", record.category, record.place, record.real_time, record.link));
+            
+            if display_name.is_empty() {
+                display_name = record.runner.to_string();
+            }
+        }
+
+        msg.channel_id.say(&ctx, &format!("Current records for player: **{}**\n{}", display_name, output)).await?;
+    } else {
+        msg.channel_id.say(&ctx, &format!("No records found for player: **{}**", runner)).await?;
+    }
+
     Ok(())
 }
